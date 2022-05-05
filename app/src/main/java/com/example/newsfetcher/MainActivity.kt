e3226f4fab8c1d0e89b78bc6dd896e44f7d03c95
nikita <nikita@mail.ru>
2022-05-05 17:26:58 +0300

    initial commit

---
+++ app/src/main/java/com/example/newsfetcher/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.newsfetcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(android.R.id.content, NewsFragment())
+            .commit()
+    }
+}
